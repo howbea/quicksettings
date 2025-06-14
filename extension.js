3f81e495391c0f7fad0ba25f0cb4c6378d7c7ba8
howbea <150657314+howbea@users.noreply.github.com>
2025-06-14 17:31:19 +0900

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,95 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+import GObject from 'gi://GObject';
+
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as SystemStatus from 'resource:///org/gnome/shell/ui/status/system.js';
+
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+import {QuickSettingsItem, QuickToggle, SystemIndicator} from 'resource:///org/gnome/shell/ui/quickSettings.js';
+
+const SoundSettingsItem = GObject.registerClass(
+class SoundSettingsItem extends QuickSettingsItem {
+    _init() {
+        super._init({
+            style_class: 'icon-button',
+            can_focus: true,
+            icon_name: 'org.gnome.Settings-sound-symbolic',
+            visible: !Main.sessionMode.isGreeter,
+        });
+
+        this.connect('clicked', () => {
+            const app = Shell.AppSystem.get_default().lookup_app('gnome-sound-panel.desktop');
+            Main.overview.hide();
+            Main.panel.closeQuickSettings();
+            app.activate();
+        });
+    }
+});
+
+const ExampleToggle = GObject.registerClass(
+class ExampleToggle extends QuickToggle {
+    constructor() {
+        super({
+            title: _('Smile'),
+            iconName: 'face-smile-symbolic',
+            toggleMode: true,
+        });
+    }
+});
+
+const ExampleIndicator = GObject.registerClass(
+class ExampleIndicator extends SystemIndicator {
+    constructor() {
+        super();
+
+        this._indicator = this._addIndicator();
+        this._indicator.iconName = 'face-smile-symbolic';
+
+        const toggle = new ExampleToggle();
+        toggle.bind_property('checked',
+            this._indicator, 'visible',
+            GObject.BindingFlags.SYNC_CREATE);
+        this.quickSettingsItems.push(toggle);
+        
+        }
+        
+});
+
+export default class QuickSettingsExampleExtension extends Extension {
+    enable() {
+        this._indicator = new ExampleIndicator();
+        Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
+        const children = Main.panel.statusArea.quickSettings.container._system._systemItem.child.get_children(); //Main.panel.statusArea.quickSettings.quickSettingsItems.child.get_children();
+        let settingsItem;
+        for (const child of children) {
+            if (child.constructor.name == "SettingsItem") {
+                    settingsItem = child;
+            }
+        }
+        settingsItem.hide();
+        //this._system._systemItem.child.hide();
+        //Main.panel.statusArea.quickSettings.container._systemItem.child.hide();
+        
+    }
+
+    disable() {
+        this._indicator.quickSettingsItems.forEach(item => item.destroy());
+        this._indicator.destroy();
+    }
+}
