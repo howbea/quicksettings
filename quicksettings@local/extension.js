48afef3703406fcc8beec93f4a0fad04d8899e81
howbea <150657314+howbea@users.noreply.github.com>
2025-11-03 10:56:46 +0900

    Add files via upload

--- quicksettings@local/extension.js
+++ quicksettings@local/extension.js
@@ -20,17 +20,25 @@ import Shell from 'gi://Shell';
 import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
+import AccountsService from 'gi://AccountsService';
+import GLib from 'gi://GLib';
 
 import * as QuickSettings from 'resource:///org/gnome/shell/ui/quickSettings.js';
-
+import * as userWidget from 'resource:///org/gnome/shell/ui/userWidget.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 const children = Main.panel.statusArea.quickSettings._system._systemItem.child.get_children();
 let settingsItem;
+let shutdownItem;
+
     for (const child of children) {
         if (child.constructor.name == "SettingsItem") {
                 settingsItem = child;
+        }
+        else if (child.constructor.name == "ShutdownItem") {
+                shutdownItem = child;
         }
     }
     
@@ -55,6 +63,56 @@ export default class QuickSettingsExampleExtension extends Extension {
     _modifySystemItem() {        
         settingsItem.hide();
         this._button();
+        this._userheader();
+        this._addmenu();
+    }
+    
+    _userheader() {
+        var userManager = AccountsService.UserManager.get_default();
+        var user = userManager.get_user(GLib.get_user_name());
+        this._avatar = new userWidget.UserWidget(user);
+        
+        shutdownItem.menu._header.hide();
+        shutdownItem.menu.box.insert_child_at_index(this._avatar, 1);
+    }
+    
+    _addmenu() {
+
+        let itemhelp = new PopupMenu.PopupMenuItem(_('Help'));
+        itemhelp.connect('activate', () => {
+        Gio.AppInfo.launch_default_for_uri_async('https://www.debian.org/support', global.create_app_launch_context(0, -1), null)
+        //Shell.AppSystem.get_default().lookup_app('yelp.desktop').activate();
+        Main.panel.closeQuickSettings();
+        }); 
+        
+        let itemsettings = new PopupMenu.PopupMenuItem(_('Settings / Updates...'));
+        itemsettings.connect('activate', () => {
+        Shell.AppSystem.get_default().lookup_app('gnome-system-panel.desktop').activate();
+        Main.panel.closeQuickSettings();
+        });        
+        
+        let itemabout = new PopupMenu.PopupMenuItem(_('About'));
+        itemabout.connect('activate', () => {
+        Shell.AppSystem.get_default().lookup_app('gnome-about-panel.desktop').activate();
+        Main.panel.closeQuickSettings();
+        });
+        
+        let itemusers = new PopupMenu.PopupMenuItem(_('User Settings'));
+        itemusers.connect('activate', () => {
+        Shell.AppSystem.get_default().lookup_app('gnome-users-panel.desktop').activate();
+        Main.panel.closeQuickSettings();
+        });        
+        
+        shutdownItem.icon_name = 'start-here-symbolic';
+        
+        shutdownItem.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem(), 0);        
+        shutdownItem.menu.addMenuItem(itemhelp, 0);        
+        shutdownItem.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem(), 0);        
+        shutdownItem.menu.addMenuItem(itemsettings, 0);        
+        shutdownItem.menu.addMenuItem(itemabout, 0);        
+        shutdownItem.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem(), 0);
+        shutdownItem.menu.addMenuItem(itemusers, 0);
+        
     }
     
     _button() {
@@ -85,5 +143,7 @@ export default class QuickSettingsExampleExtension extends Extension {
         this._EButton = null;
         if (Main.sessionMode.currentMode !== 'unlock-dialog')
         settingsItem.show();
+        this._avatar.destroy();
+        this._avatar = null;
     }
 }
